a38817904446d2247f2c1f6830a46557603343e4
Almir Ximenes <aximenesjr@gmail.com>
2023-01-11 08:21:47 -0300

    modified:   DesafioPOO.csproj to .NET7.0 Implementado atributos na classe abstrata Smartphone.cs e seus construtores Implementado overrides em Nokia.cs e Iphone.cs Elaborada rotina de testes no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("99663322", "nokia1", "11111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("22663322", "iphone1", "22222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
